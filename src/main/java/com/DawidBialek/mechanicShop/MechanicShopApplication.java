10c3efcc30f5188cfad47aa8de994e3fdc77ee82
DawidBialek <dd.bialek@gmail.com>
2020-11-27 15:06:18 +0100

    Init commit
    Spring boot initalization

---
+++ src/main/java/com/DawidBialek/mechanicShop/MechanicShopApplication.java
@@ -0,0 +1,13 @@
+package com.DawidBialek.mechanicShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechanicShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DawidBialek/mechanicShop/MechanicShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DawidBialek.mechanicShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanicShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
